5ea4d9a0df8b3452f5e91024b45de9fae01a8307
Simon Twilight <121682528+TG-Twilight@users.noreply.github.com>
2025-06-29 16:05:28 +0800

    Update header.php

--- custom/PC/header.php
+++ custom/PC/header.php
@@ -17,13 +17,14 @@ foreach ($domains as $domain): ?>
 <?php endif; ?>
 <title>
 <?php if ($this->is('index')): ?>
+<?php $this->options->title(); ?>
 <?php else: ?>
 <?php $this->archiveTitle([
-            'category' => _t('%s'),
-            'search'   => _t('包含关键字 %s 的文章'),
-            'tag'      => _t('标签 %s 下的文章'),
-            'author'   => _t('%s 发布的文章')
-        ], '', ' - '); ?><?php $this->options->title(); ?>
+    'category' => _t('%s'),
+    'search'   => _t('包含关键字 %s 的文章'),
+    'tag'      => _t('标签 %s 下的文章'),
+    'author'   => _t('%s 发布的文章')
+], '', ' - '); ?><?php $this->options->title(); ?>
 <?php endif; ?>
 </title>
 <link href="<?php $this->options->themeUrl('/assets/sdk/animate.compat.css'); ?>" rel="stylesheet"><!--动画效果-->
